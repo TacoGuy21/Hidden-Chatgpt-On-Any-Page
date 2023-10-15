43a6c6e7cfa10767c7307a43902dd0db34fff2f9
Tacogamerman <119009502+Tacogamerman@users.noreply.github.com>
2023-10-15 19:20:15 -0400

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,6 +1,6 @@
 javascript:(function() {
   var iframe = document.createElement('iframe');
-  iframe.setAttribute('src', 'https://Bing.com');
+  iframe.setAttribute('src', 'https://gptchatly.com/');
   iframe.style.position = 'fixed';
   iframe.style.top = '50%';
   iframe.style.left = '50%';
